678a75f493231a463e2cd313509b501d08772409
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-11-23 16:22:10 +1000

    rebuild creep creation

--- creeps.creation.js
+++ creeps.creation.js
@@ -1,72 +1,49 @@
 var creepsCreation = {
   run: function() {
-
+    // clear memory
     for (var name in Memory.creeps) {
       if (!Game.creeps[name]) {
         delete Memory.creeps[name];
         // console.log('Clearing non-existing creep memory:', name);
       }
     }
-
+    // get creeps numbers
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    // console.log('Harvesters: ' + (harvesters.length % 2));
-
-    if (harvesters.length < 3) {
-      var n = 0;
-      if (harvesters.length % 2 == 0) {
-        n = 1;
-      }
-      //    [ WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE ]
-      var newName = Game.spawns['House1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester', source: n});
-      // console.log('Spawning new harvester: ' + newName);
-    }
-
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    // console.log('Upgraders: ' + upgraders.length);
-
-    if (upgraders.length < 2) {
-      var n = 0;
-      if (upgraders.length % 2 == 0) {
-        n = 1;
-      }
-      var newName = Game.spawns['House1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader', source: n});
-      // console.log('Spawning new upgrader: ' + newName);
-    }
-
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    // console.log('Upgraders: ' + upgraders.length);
-
-    if (builders.length < 2) {
-      var n = 0;
-      if (builders.length % 2 == 0) {
-        n = 1;
-      }
-      var newName = Game.spawns['House1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'builder', source: n});
-      // console.log('Spawning new builders: ' + newName);
-    }
-
-    /*var soldiers = _.filter(Game.creeps, (creep) => creep.memory.role ==
-    'soldier');
-    //console.log('soldiers: ' + soldiers.length);
-
-    if (soldiers.length < 2) {
-        var newName = Game.spawns['House1'].createCreep([TOUGH, ATTACK, ATTACK,
-    MOVE, MOVE], undefined, { role: 'soldier' });
-        //console.log('Spawning new soldier: ' + newName);
-    }*/
-
     var mechanics = _.filter(Game.creeps, (creep) => creep.memory.role == 'mechanic');
-    // console.log('Upgraders: ' + upgraders.length);
+    // creeps selection
+    if (harvesters.length < 3) {
+      createCreep(name, 'harvester');
+    } else if (upgraders.length < 2) {
+      createCreep(name, 'upgrader');
+    } else if (builders.length < 3) {
+      createCreep(name, 'builder');
+    } else if (mechanics.length < 2) {
+      createCreep(name, 'mechanic');
+    } else {
+      /*var soldiers = _.filter(Game.creeps, (creep) => creep.memory.role ==
+      'soldier');
+      //console.log('soldiers: ' + soldiers.length);
 
-    if (mechanics.length < 2) {
-      var n = 0;
-      if (mechanics.length % 2 == 0) {
-        n = 1;
-      }
-      var newName = Game.spawns['House1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'mechanic', source: n});
-      // console.log('Spawning new builders: ' + newName);
+      if (soldiers.length < 2) {
+          var newName = Game.spawns['House1'].createCreep([TOUGH, ATTACK, ATTACK,
+      MOVE, MOVE], undefined, { role: 'soldier' });
+          //console.log('Spawning new soldier: ' + newName);
+      }*/
     }
   }
+};
+
+var createCreep = function(name, role) {
+  // calculate source
+  var n = 0;
+  if (Game.creeps.length % 2 == 0) {
+    n = 1;
+  }
+  //    [ WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE ]
+  var newName = Game.spawns['House1'].createCreep([WORK, CARRY, MOVE], undefined, {role: role, source: n});
+  // console.log('Spawning new harvester: ' + newName);
 };
 
 module.exports = creepsCreation;
