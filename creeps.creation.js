632200aafa0b4a586b0792083b6e48dc73f2760a
Ernesto La Fontaine <mail@pajarraco.com>
2016-11-24 01:32:46 +1000

    fix miner source count

--- creeps.creation.js
+++ creeps.creation.js
@@ -23,7 +23,10 @@ var creepsCreation = {
     } else if (mechanics.length < 2) {
       createCreep(name, 'mechanic');
     } else if (miners.length < 2) {
-      var n = miners.length;
+      var n = 0;
+      if (miners[0].memory.source == 0) {
+        n = 1;
+      }
       var newName = Game.spawns['House1'].createCreep(
           [WORK, WORK, WORK, WORK, CARRY, MOVE], undefined, {role: 'miner', source: n});
     } else {
