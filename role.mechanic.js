f21f231544ab9d539333b386955862ab7f1df005
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-11-23 15:36:45 +1000

    fix repairing

--- role.mechanic.js
+++ role.mechanic.js
@@ -1,18 +1,27 @@
 var roleMechanic = {
   run: function(creep) {
-    if (creep.carry.energy < creep.carryCapacity) {
-      var sources = creep.room.find(FIND_SOURCES);
-      var i = creep.memory.source;
-      if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[i]);
-      }
-    } else {
+
+    if (creep.memory.repairing && creep.carry.energy == 0) {
+      creep.memory.repairing = false;
+      creep.say('harvesting');
+    }
+    if (!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.repairing = true;
+      creep.say('repairing');
+    }
+    if (creep.memory.repairing) {
       var closestDamagedStructure =
           creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax});
       if (closestDamagedStructure) {
         if (creep.repair(closestDamagedStructure) == ERR_NOT_IN_RANGE) {
           creep.moveTo(closestDamagedStructure);
         }
+      }
+    } else {
+      var sources = creep.room.find(FIND_SOURCES);
+      var i = creep.memory.source;
+      if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[i]);
       }
     }
   }
