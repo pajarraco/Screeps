18f76e357bc638350679ed5d1acf10c6b500bb11
Ernesto La Fontaine <mail@pajarraco.com>
2016-11-23 23:39:25 +1000

    miner

---
+++ role.miner.js
@@ -0,0 +1,20 @@
+var roleMiner = {
+  run: function(creep) {
+    if (creep.carry.energy < creep.carryCapacity) {
+      var sources = creep.room.find(FIND_SOURCES);
+      var i = creep.memory.source;
+      if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[i]);
+      }
+    } else {
+      var containers = creep.room.find(
+          FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0});
+      if (containers.length > 0) {
+        if (creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(containers[0]);
+        }
+      }
+    }
+  }
+};
+module.exports = roleMiner;
