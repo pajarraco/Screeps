d8d6233e363506e1d9949de4d1be5bc7d69f323b
Ernesto La Fontaine <mail@pajarraco.com>
2016-11-24 21:27:52 +1000

    test

--- role.delivery.js
+++ role.delivery.js
@@ -8,17 +8,16 @@ var roleDelivery = {
       creep.memory.delivering = true;
       creep.say('delivering');
     }
-    var containers =
-        creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER});
+    var containers = creep.room.find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER});
     var pickupTarget = containers[0];
     var deliveryTarget = containers[0];
-    for (var container in containers) {
+    containers.forEach(function(container) {
       if (container.store[RESOURCE_ENERGY] > pickupTarget.store[RESOURCE_ENERGY]) {
         pickupTarget = container;
       } else if (container.store[RESOURCE_ENERGY] < deliveryTarget.store[RESOURCE_ENERGY]) {
         deliveryTarget = container;
       }
-    }
+    });
     if (creep.memory.delivering) {
       if (creep.transfer(deliveryTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(deliveryTarget);
