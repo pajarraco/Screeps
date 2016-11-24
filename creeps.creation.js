7b15d1c44b9baa83b6c820f64706620566094d8e
Ernesto La Fontaine <mail@pajarraco.com>
2016-11-25 00:09:29 +1000

    reorder creation

--- creeps.creation.js
+++ creeps.creation.js
@@ -10,19 +10,15 @@ var creepsCreation = {
     // get creeps numbers
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    var mechanics = _.filter(Game.creeps, (creep) => creep.memory.role == 'mechanic');
     var miners = _.filter(Game.creeps, (creep) => creep.memory.role == 'miner');
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var deliveries = _.filter(Game.creeps, (creep) => creep.memory.role == 'delivery');
+    var mechanics = _.filter(Game.creeps, (creep) => creep.memory.role == 'mechanic');
     // creeps selection
     if (harvesters.length < 3) {
       createCreep(name, 'harvester');
     } else if (upgraders.length < 3) {
       createCreep(name, 'upgrader');
-    } else if (builders.length < 3) {
-      createCreep(name, 'builder');
-    } else if (mechanics.length < 5) {
-      createCreep(name, 'mechanic');
     } else if (miners.length < 2) {
       var n = 0;
       if (miners.length > 0) {
@@ -32,8 +28,12 @@ var creepsCreation = {
       }
       var newName = Game.spawns['Spawn1'].createCreep(
           [WORK, WORK, WORK, WORK, CARRY, MOVE], undefined, {role: 'miner', source: n});
+    } else if (builders.length < 3) {
+      createCreep(name, 'builder');
     } else if (deliveries.length < 2) {
       var newName = Game.spawns['Spawn1'].createCreep([CARRY, MOVE, MOVE, MOVE, MOVE], undefined, {role: 'delivery'});
+    } else if (mechanics.length < 5) {
+      createCreep(name, 'mechanic');
     } else {
       var soldiers = _.filter(Game.creeps, (creep) => creep.memory.role == 'soldier');
       if (soldiers.length < 5) {
