0a213d435705aecbac4d5014467438f5c76970e9
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-11-25 10:42:10 +1000

    rampart

--- role.soldier.js
+++ role.soldier.js
@@ -4,17 +4,18 @@ var roleSoldier = {
   run: function(creep) {
 
     var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    console.log('soldier creep: ' + creep + ' hostile ' + closestHostile);
+    // console.log('soldier creep: ' + creep + ' hostile ' + closestHostile);
     if (closestHostile) {
       Game.spawns['Spawn1'].room.controller.activateSafeMode();
       if (creep.attack(closestHostile) == ERR_NOT_IN_RANGE) {
         creep.moveTo(closestHostile);
       }
     } else {
-      var containers =
+      var rampart =
           creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_RAMPART});
-      if (containers) {
-        creep.moveTo(containers);
+      console.log('rampart', rampart);
+      if (rampart) {
+        creep.moveTo(rampart);
       }
     }
   }
