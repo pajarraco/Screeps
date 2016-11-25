40d53c01d118ef5d2ebc99cb04f38dd056354a23
Ernesto La Fontaine <mail@pajarraco.com>
2016-11-25 17:00:13 +1000

    tower

--- main.js
+++ main.js
@@ -6,24 +6,12 @@ var roleMiner = require('role.miner');
 var roleDelivery = require('role.delivery');
 var roleSoldier = require('role.soldier');
 var creepsCreation = require('creeps.creation');
+var towerActivity = require('tower.activity');
 
 module.exports.loop = function() {
 
-  var tower = Game.getObjectById('5837ad49f4ec434d64dc5214');
-
-  if (tower) {
-    var closestDamagedStructure =
-        tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax});
-    if (closestDamagedStructure) {
-      tower.repair(closestDamagedStructure);
-    }
-    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (closestHostile) {
-      tower.attack(closestHostile);
-    }
-  }
-
   creepsCreation.run();
+  towerActivity.run();
 
   for (var name in Game.creeps) {
     var creep = Game.creeps[name];
--- role.soldier.js
+++ role.soldier.js
@@ -6,13 +6,13 @@ var findEmptyRampart = function(creep) {
   ramparts.forEach(function(rampart) {
     var emptyRampart = true;
     soldiers.forEach(function(soldier) {
-      console.log('soldier', soldier.pos);
+      // console.log('soldier', soldier.pos);
       if (rampart.pos.x == soldier.pos.x && rampart.pos.y == soldier.pos.y) {
-        console.log('pos ', rampart.pos, soldier.pos);
+        // console.log('pos ', rampart.pos, soldier.pos);
         emptyRampart = false;
       }
     });
-    console.log('empty rampart ', rampart.pos, emptyRampart);
+    // console.log('empty rampart ', rampart.pos, emptyRampart);
     if (emptyRampart) {
       returnRampart = rampart;
       return false;
@@ -29,14 +29,12 @@ var roleSoldier = {
     var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
     // console.log('soldier creep: ' + creep + ' hostile ' + closestHostile);
     if (closestHostile) {
-      Game.spawns['Spawn1'].room.controller.activateSafeMode();
       if (creep.attack(closestHostile) == ERR_NOT_IN_RANGE) {
         creep.moveTo(closestHostile);
       }
     } else {
       rampart = findEmptyRampart(creep);
-
-      console.log('rampart: ', rampart);
+      // console.log('rampart: ', rampart);
       if (rampart) {
         creep.moveTo(rampart);
       }
