e0c3dfa0269ddf334ab6fd9caf8ac0713d3bbbef
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-11-25 13:21:35 +1000

    tower

--- main.js
+++ main.js
@@ -9,22 +9,19 @@ var creepsCreation = require('creeps.creation');
 
 module.exports.loop = function() {
 
-  // var tower = Game.getObjectById('a7aeb58f7595cf9dc437f700');
+  var tower = Game.getObjectById('5837ad49f4ec434d64dc5214');
 
-  /*if (tower) {
-      var closestDamagedStructure =
-  tower.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: (structure) => structure.hits < structure.hitsMax
-      });
-      if (closestDamagedStructure) {
-          tower.repair(closestDamagedStructure);
-      }
-
-      var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-      if (closestHostile) {
-          tower.attack(closestHostile);
-      }
-  }*/
+  if (tower) {
+    var closestDamagedStructure =
+        tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax});
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
 
   creepsCreation.run();
 
