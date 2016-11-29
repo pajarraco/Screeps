b88741b0ada803572b6164cb3b18057b13ea2c1b
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-11-29 13:07:40 +1000

    explorer work

--- role.explorer.js
+++ role.explorer.js
@@ -3,16 +3,16 @@ var roleExplorer = {
   /** @param {Creep} creep **/
   run: function(creep) {
 
-    if (creep.carry.energy < creep.carryCapacity) {
-      if (creep.pos.roomName == Game.flags['LeftRoom'].pos.roomName) {
-        var source = creep.pos.findClosestByRange(FIND_SOURCES);
-        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(source);
-        }
-      } else {
-        creep.moveTo(Game.flags['LeftRoom']);
-      }
-    } else {
+    if (creep.memory.transferring && creep.carry.energy == 0) {
+      creep.memory.transferring = false;
+      creep.say('harvesting');
+    }
+    if (!creep.memory.transferring && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.transferring = true;
+      creep.say('transferring');
+    }
+
+    if (creep.memory.transferring) {
       var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
       if (target) {
         if (creep.build(target) == ERR_NOT_IN_RANGE) {
@@ -22,7 +22,6 @@ var roleExplorer = {
         var depositTargets = Game.rooms['E37S69'].find(
             FIND_STRUCTURES,
             {filter: (s) => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] < s.storeCapacity});
-
         if (depositTargets.length > 0) {
           if (creep.transfer(depositTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(depositTargets[0]);
@@ -30,6 +29,15 @@ var roleExplorer = {
         } else {
           roleTowerkeeper.run(creep);
         }
+      }
+    } else {
+      if (creep.pos.roomName == Game.flags['LeftRoom'].pos.roomName) {
+        var source = creep.pos.findClosestByRange(FIND_SOURCES);
+        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
+      } else {
+        creep.moveTo(Game.flags['LeftRoom']);
       }
     }
   }
