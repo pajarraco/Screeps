ccb2903477a54e7f681f2ed870359550013b9d41
Ernesto La Fontaine <mail@pajarraco.com>
2016-12-05 20:51:39 +1000

    < 5

--- role.explorer.js
+++ role.explorer.js
@@ -53,7 +53,7 @@ var roleExplorer = {
       var creepsOnRoom = _.filter(Game.creeps, function(c) {
                             return c.room.name == creep.pos.roomName
                           }).length;
-      if (creepsOnRoom > 5) {
+      if (creepsOnRoom <= 5) {
         if (creep.pos.roomName == Game.flags['LeftRoom'].pos.roomName) {
           var source = creep.pos.findClosestByRange(FIND_SOURCES);
           if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
