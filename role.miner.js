67a23713b78bb528d842947c8aa89a10837886e9
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-05 14:20:29 +1000

    miner

--- role.miner.js
+++ role.miner.js
@@ -2,13 +2,14 @@ var roleMiner = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-    if (creep.carry.energy < creep.carryCapacity) {
-      var sources = creep.room.find(FIND_SOURCES);
-      var i = creep.memory.source;
-      if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[i]);
-      }
-    } /*else {
+    // if (creep.carry.energy < creep.carryCapacity) {
+    var sources = creep.room.find(FIND_SOURCES);
+    var i = creep.memory.source;
+    if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[i]);
+    }
+    //}
+    /*else {
       var container = creep.pos.findClosestByRange(
           FIND_STRUCTURES,
           {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] < s.storeCapacity});
