da5534b2913d42c1ea83acb0981e9adb0628d79d
Ernesto La Fontaine <mail@pajarraco.com>
2016-12-05 21:17:49 +1000

    fix explorer

--- role.explorer.js
+++ role.explorer.js
@@ -11,62 +11,62 @@ var roleExplorer = {
       if (creep.attack(closestHostile) == ERR_NOT_IN_RANGE) {
         creep.moveTo(closestHostile);
       }
-    }
-
-    // attack hostile structure
-    var closestHostileStructures = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-    if (closestHostileStructures) {
-      if (creep.attack(closestHostileStructures) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(closestHostileStructures);
-      }
-    }
-
-    // find resources
-    if (creep.memory.transferring && creep.carry.energy == 0) {
-      creep.memory.transferring = false;
-      creep.say('harvesting');
-    }
-    if (!creep.memory.transferring && creep.carry.energy == creep.carryCapacity) {
-      creep.memory.transferring = true;
-      creep.say('transferring');
-    }
-
-    if (creep.memory.transferring) {
-      var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-      if (target) {
-        if (creep.build(target) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target);
-        }
-      } else {
-        var depositTargets = Game.rooms['E37S69'].find(
-            FIND_STRUCTURES,
-            {filter: (s) => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] < s.storeCapacity});
-        if (depositTargets.length > 0) {
-          if (creep.transfer(depositTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(depositTargets[0]);
-          }
-        } else {
-          roleTowerkeeper.run(creep);
-        }
-      }
     } else {
-      if (creep.memory.source == 0) {
-        if (creep.pos.roomName == Game.flags['LeftRoom'].pos.roomName) {
-          var source = creep.pos.findClosestByRange(FIND_SOURCES);
-          if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
-          }
-        } else {
-          creep.moveTo(Game.flags['LeftRoom']);
+      // attack hostile structure
+      var closestHostileStructures = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+      if (closestHostileStructures) {
+        if (creep.attack(closestHostileStructures) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(closestHostileStructures);
         }
       } else {
-        if (creep.pos.roomName == Game.flags['TopRoom'].pos.roomName) {
-          var source = creep.pos.findClosestByRange(FIND_SOURCES);
-          if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
+        // find resources
+        if (creep.memory.transferring && creep.carry.energy == 0) {
+          creep.memory.transferring = false;
+          creep.say('harvesting');
+        }
+        if (!creep.memory.transferring && creep.carry.energy == creep.carryCapacity) {
+          creep.memory.transferring = true;
+          creep.say('transferring');
+        }
+
+        if (creep.memory.transferring) {
+          var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+          if (target) {
+            if (creep.build(target) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(target);
+            }
+          } else {
+            var depositTargets = Game.rooms['E37S69'].find(
+                FIND_STRUCTURES,
+                {filter: (s) => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] < s.storeCapacity});
+            if (depositTargets.length > 0) {
+              if (creep.transfer(depositTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(depositTargets[0]);
+              }
+            } else {
+              roleTowerkeeper.run(creep);
+            }
           }
         } else {
-          creep.moveTo(Game.flags['TopRoom']);
+          if (creep.memory.source == 0) {
+            if (creep.pos.roomName == Game.flags['LeftRoom'].pos.roomName) {
+              var source = creep.pos.findClosestByRange(FIND_SOURCES);
+              if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+              }
+            } else {
+              creep.moveTo(Game.flags['LeftRoom']);
+            }
+          } else {
+            if (creep.pos.roomName == Game.flags['TopRoom'].pos.roomName) {
+              var source = creep.pos.findClosestByRange(FIND_SOURCES);
+              if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+              }
+            } else {
+              creep.moveTo(Game.flags['TopRoom']);
+            }
+          }
         }
       }
     }
