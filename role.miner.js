3c1a15d4b3e829d3f4dc602b20d968ccf8a5e62b
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-05 14:31:11 +1000

    container move to

--- role.miner.js
+++ role.miner.js
@@ -7,9 +7,11 @@ var roleMiner = {
     var i = creep.memory.source;
     if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
       creep.moveTo(sources[i]);
+
+      var container = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {filter: {structureType: STRUCTURE_CONTAINER}});
+      console.log(container);
     }
-    //}
-    /*else {
+    /*} else {
       var container = creep.pos.findClosestByRange(
           FIND_STRUCTURES,
           {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] < s.storeCapacity});
