69f8a1447af4d72af6bd7e278e8f707149823cc8
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-05 14:07:36 +1000

    fdf

--- structure.link.js
+++ structure.link.js
@@ -3,9 +3,9 @@ var structureLink = {
   /** @param  {Room} room  **/
   run: function(room) {
 
-    var linkFrom = room.lookForAt('structure', 29, 39)[0];
-    var linkTo = room.lookForAt('structure', 32, 26)[0];
-    // linkFrom.transferEnergy(linkTo);
+    var linkFrom = room.lookForAt('structure', 29, 39)[1];
+    var linkTo = room.lookForAt('structure', 32, 26)[1];
+    linkFrom.transferEnergy(linkTo);
   }
 };
 module.exports = structureLink;
