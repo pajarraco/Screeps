cc1fe932244ac83eb31cad8cd1cc067537d417e0
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-05 13:22:40 +1000

    defense first

--- tower.activity.js
+++ tower.activity.js
@@ -4,15 +4,16 @@ var towerActivity = {
 
     var tower = Game.getObjectById('5837ad49f4ec434d64dc5214');
     if (tower) {
-      var closestDamagedStructure = tower.pos.findClosestByRange(
-          FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax && structure.hits < 250000});
-      if (closestDamagedStructure && tower.energy > 500) {
-        tower.repair(closestDamagedStructure);
-      }
       var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
       if (closestHostile) {
         Game.spawns['Spawn1'].room.controller.activateSafeMode();
         tower.attack(closestHostile);
+      } else {
+        var closestDamagedStructure = tower.pos.findClosestByRange(
+            FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax && structure.hits < 250000});
+        if (closestDamagedStructure && tower.energy > 500) {
+          tower.repair(closestDamagedStructure);
+        }
       }
     }
   }
