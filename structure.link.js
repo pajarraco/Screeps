a1afbcb609e3b74c85c08bb72958f94a17824b7d
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-05 14:09:14 +1000

    fix

--- structure.link.js
+++ structure.link.js
@@ -5,7 +5,7 @@ var structureLink = {
 
     var linkFrom = room.lookForAt('structure', 29, 39)[1];
     var linkTo = room.lookForAt('structure', 32, 26)[1];
-    linkFrom.transferEnergy(linkTo);
+    // linkFrom.transferEnergy(linkTo);
   }
 };
 module.exports = structureLink;
