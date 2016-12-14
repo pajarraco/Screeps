9948411332876aa29b9f3539367db8b455548330
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-14 16:12:58 +1000

    miner

--- spawn.creation.js
+++ spawn.creation.js
@@ -55,7 +55,7 @@ var creepsCreation = {
             });
           }
           if (spawn.name == 'Spawn1') {
-            spawn.createCreep([WORK, WORK, WORK, WORK, WORK, WORK, WORK, MOVE], undefined, {role: 'miner', source: n});
+            spawn.createCreep([WORK, WORK, WORK, WORK, WORK, WORK, MOVE], undefined, {role: 'miner', source: n});
           } else {
             spawn.createCreep([WORK, WORK, WORK, WORK, MOVE], undefined, {role: 'miner', source: n});
           }
