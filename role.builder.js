c2e1f10a4567d13b91a3a45a7cd55e9e75ea5631
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-20 14:15:42 +1000

    add drop energy

--- role.builder.js
+++ role.builder.js
@@ -32,23 +32,30 @@ var roleBuilder = {
         }
       }
     } else {
-      var storages = creep.pos.findClosestByRange(
-          FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 300});
-      if (storages) {
-        if (creep.withdraw(storages, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(storages);
+      var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+      if (target) {
+        if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
         }
       } else {
-        var containers = creep.pos.findClosestByRange(
-            FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0});
-        if (containers) {
-          if (creep.withdraw(containers, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(containers);
+        var storages = creep.pos.findClosestByRange(
+            FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 300});
+        if (storages) {
+          if (creep.withdraw(storages, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(storages);
           }
         } else {
-          var sources = creep.pos.findClosestByRange(FIND_SOURCES);
-          if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources);
+          var containers = creep.pos.findClosestByRange(
+              FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0});
+          if (containers) {
+            if (creep.withdraw(containers, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(containers);
+            }
+          } else {
+            var sources = creep.pos.findClosestByRange(FIND_SOURCES);
+            if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(sources);
+            }
           }
         }
       }
