d44b04a15dbd792ccc8bd99bdef888a52020004f
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-21 16:23:47 +1000

    conquest

--- role.conquest.js
+++ role.conquest.js
@@ -27,11 +27,9 @@ var roleConquest = {
       }
     } else {
       if (creep.memory.source == 0) {
-        creep.moveTo(Game.flags['TopRoom']);
-      } else if (creep.memory.source == 1) {
         creep.moveTo(Game.flags['LeftRoom']);
-      } else {
-        creep.moveTo(Game.flags['LeftRoom2']);
+      } else if (creep.memory.source == 1) {
+        creep.moveTo(Game.flags['TopRoom']);
       }
     }
   }
