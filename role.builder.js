9f19d2eaa82150d5811aab356fdb3ad8cddf8019
Ernesto La Fontaine <ernesto@cwad.com.au>
2016-12-21 12:25:06 +1000

    resouce

--- role.builder.js
+++ role.builder.js
@@ -14,52 +14,53 @@ var roleBuilder = {
         }
       }
     }
-
-    if (creep.memory.building && creep.carry.energy == 0) {
-      creep.memory.building = false;
-      creep.say('harvesting');
-    }
-    if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-      creep.memory.building = true;
-      creep.say('building');
-    }
-
-    if (creep.memory.building) {
-      var targets = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-      if (targets) {
-        if (creep.build(targets) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets);
-        }
-      } else {
-        var closestDamagedStructure =
-            creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.hits < s.hitsMax && s.hits < 3000});
-        if (closestDamagedStructure) {
-          if (creep.repair(closestDamagedStructure) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(closestDamagedStructure);
-          }
-        } else {
-          roleHarvester.run(creep);
-        }
+    els {
+      if (creep.memory.building && creep.carry.energy == 0) {
+        creep.memory.building = false;
+        creep.say('harvesting');
       }
-    } else {
-      var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
-      if (target) {
-        if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target);
-        }
-      } else {
-        var storages = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: (s) => (s.structureType == STRUCTURE_STORAGE || s.structureType == STRUCTURE_CONTAINER) &&
-              s.store[RESOURCE_ENERGY] > 300
-        });
-        if (storages) {
-          if (creep.withdraw(storages, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(storages);
+      if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.building = true;
+        creep.say('building');
+      }
+
+      if (creep.memory.building) {
+        var targets = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+        if (targets) {
+          if (creep.build(targets) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets);
           }
         } else {
-          var sources = creep.pos.findClosestByRange(FIND_SOURCES);
-          if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources);
+          var closestDamagedStructure =
+              creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.hits < s.hitsMax && s.hits < 3000});
+          if (closestDamagedStructure) {
+            if (creep.repair(closestDamagedStructure) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(closestDamagedStructure);
+            }
+          } else {
+            roleHarvester.run(creep);
+          }
+        }
+      } else {
+        var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+        if (target) {
+          if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+          }
+        } else {
+          var storages = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (s) => (s.structureType == STRUCTURE_STORAGE || s.structureType == STRUCTURE_CONTAINER) &&
+                s.store[RESOURCE_ENERGY] > 300
+          });
+          if (storages) {
+            if (creep.withdraw(storages, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(storages);
+            }
+          } else {
+            var sources = creep.pos.findClosestByRange(FIND_SOURCES);
+            if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(sources);
+            }
           }
         }
       }
