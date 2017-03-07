3ea897c38423917151fa119159d21445c5718ccc
Ernesto La Fontaine <ernesto@cwad.com.au>
2017-03-07 15:13:30 +1000

    miners fix

--- role.miner.js
+++ role.miner.js
@@ -6,14 +6,15 @@ var roleMiner = {
     var i = creep.memory.source;
     if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
       creep.moveTo(sources[i]);
-    }
-    var links = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType: STRUCTURE_LINK}});
-    if (links.length > 0) {
-      creep.transfer(links[0], RESOURCE_ENERGY);
-    }
-    var containers = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType: STRUCTURE_CONTAINER}});
-    if (containers.length > 0) {
-      creep.moveTo(containers[0]);
+    } else {
+      var links = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType: STRUCTURE_LINK}});
+      if (links.length > 0) {
+        creep.transfer(links[0], RESOURCE_ENERGY);
+      }
+      var containers = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType: STRUCTURE_CONTAINER}});
+      if (containers.length > 0) {
+        creep.moveTo(containers[0]);
+      }
     }
   }
 };
