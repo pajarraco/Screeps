69d694417ec2e7ca3661ec5fc6e48b6d9a25287d
CAROLINA\Ernesto <mail@pajarraco.com>
2017-07-15 11:40:45 +1000

    no message

--- role.helper.js
+++ role.helper.js
@@ -16,45 +16,44 @@ const roleHelper = {
     }
 
     if (creep.memory.transferring) {
-    //   const labs = creep.room.find(
-    //     FIND_STRUCTURES, {
-    //       filter: (s) => s.structureType == STRUCTURE_LAB
-    //     });
+      //   const labs = creep.room.find(
+      //     FIND_STRUCTURES, {
+      //       filter: (s) => s.structureType == STRUCTURE_LAB
+      //     });
       const terminal = creep.room.find(
         FIND_STRUCTURES, {
           filter: (s) => s.structureType == STRUCTURE_TERMINAL
         });
-    //   if (labs.length > 0) {
-        _.each(creep.carry, (resource, key) => {
-          if (key === 'O') {
-            for (const resourceType in creep.carry) {
-              creep.drop(resourceType);
-            }
-            // if (terminal.length > 0) {
-            //     if (creep.transfer(terminal[0], key) == ERR_NOT_IN_RANGE) {
-            //         creep.moveTo(terminal[0]);
-            //     }
-            // }
-          } else {
-              console.log('terminal');
-            if (creep.memory.target === 'terminal') {
-              if (terminal.length > 0) {
-                if (creep.transfer(terminal[0], key)) {
-                  creep.moveTo(terminal[0]);
-                }
-              }
-            } else {
-              //   const myLab = _.min(labs, (lab) => lab.energy);
-              //   let transfer = creep.transfer(myLab, key);
-              //   if (transfer == ERR_NOT_IN_RANGE) {
-              //     creep.moveTo(myLab);
-              //   } else if (transfer == ERR_FULL) {
-              //     creep.memory.target = 'terminal';
-              //   }
+      //   if (labs.length > 0) {
+      _.each(creep.carry, (resource, key) => {
+        if (key === 'O') {
+          for (const resourceType in creep.carry) {
+            creep.drop(resourceType);
+          }
+          // if (terminal.length > 0) {
+          //     if (creep.transfer(terminal[0], key) == ERR_NOT_IN_RANGE) {
+          //         creep.moveTo(terminal[0]);
+          //     }
+          // }
+        } else {
+          // if (creep.memory.target === 'terminal') {
+          if (terminal.length > 0) {
+            if (creep.transfer(terminal[0], key)) {
+              creep.moveTo(terminal[0]);
             }
           }
-        });
-    //   }
+          // } else {
+          //   const myLab = _.min(labs, (lab) => lab.energy);
+          //   let transfer = creep.transfer(myLab, key);
+          //   if (transfer == ERR_NOT_IN_RANGE) {
+          //     creep.moveTo(myLab);
+          //   } else if (transfer == ERR_FULL) {
+          //     creep.memory.target = 'terminal';
+          //   }
+          // }
+        }
+      });
+      //   }
     } else {
       const storage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
         filter: (s) => s.structureType == STRUCTURE_STORAGE
