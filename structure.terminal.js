57f7239b347b7f1ddaad65f7f1eff806450a80bb
CAROLINA\Ernesto <mail@pajarraco.com>
2017-07-15 12:04:35 +1000

    no message

--- structure.terminal.js
+++ structure.terminal.js
@@ -17,7 +17,7 @@ const structureTerminal = {
         let send = terminal1.send(RESOURCE_ENERGY, 100, home4, 'Send O');
         console.log('terminal send', send);
         
-        let send2 = terminal2.send(RESOURCE_OXYGEN, 100, home4, 'Send H');
+        let send2 = terminal2.send(RESOURCE_OXYGEN, 100, home3, 'Send H');
         console.log('terminal send2', send2);
 
     }
