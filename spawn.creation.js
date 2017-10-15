efe143bff64961928bf343045328d5da20a88a5f
Ernesto La Fontaine <mail@pajarraco.com>
2017-10-16 09:48:08 +1000

    update

--- spawn.creation.js
+++ spawn.creation.js
@@ -105,41 +105,41 @@ const creepsCreation = {
       // Miner
       const miners = _.filter(Game.creeps, (creep) => creep.memory.role == 'miner' && creep.room.name == spawn.room.name);
       if (miners.length < 2) {
-        if (spawn.room.name !== home5) {
+        // if (spawn.room.name !== home5) {
           createNewCreep(spawn, name, minerBody, 'miner', miners);
-        } else {
-          createNewCreep(spawn, name, minerBodyLow, 'miner', miners);
-        }
+        // } else {
+          // createNewCreep(spawn, name, minerBodyLow, 'miner', miners);
+        // }
       } else {
         //
         // upgrader
         const upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.room.name == spawn.room.name);
         if (upgraders.length < 1) {
-          if (spawn.room.name !== home5) {
+          // if (spawn.room.name !== home5) {
             createNewCreep(spawn, name, upgraderBody, 'upgrader', upgraders);
-          } else {
-            createNewCreep(spawn, name, upgraderBodyLow, 'upgrader', upgraders);
-          }
+          // } else {
+          //   createNewCreep(spawn, name, upgraderBodyLow, 'upgrader', upgraders);
+          // }
         } else {
           //
           // Tower keeper
           const towerkeepers = _.filter(Game.creeps, (creep) => creep.memory.role == 'towerkeeper' && creep.room.name == spawn.room.name);
           if (towerkeepers.length < 1) {
-            if (spawn.room.name !== home5) {
+            // if (spawn.room.name !== home5) {
               createNewCreep(spawn, name, towerkeeperBody, 'towerkeeper', towerkeepers);
-            } else {
-              createNewCreep(spawn, name, towerkeeperBodyLow, 'towerkeeper', towerkeepers);
-            }
+            // } else {
+            //   createNewCreep(spawn, name, towerkeeperBodyLow, 'towerkeeper', towerkeepers);
+            // }
           } else {
             //
             // Builder
             const builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder' && creep.room.name == spawn.room.name);
             if (builders.length < 1) {
-              if (spawn.room.name !== home5) {
+              // if (spawn.room.name !== home5) {
                 createNewCreep(spawn, name, builderBody, 'builder', builders);
-              } else {
-                createNewCreep(spawn, name, builderBodyLow, 'builder', builders);
-              }
+              // } else {
+              //   createNewCreep(spawn, name, builderBodyLow, 'builder', builders);
+              // }
             } else {
               //
               // Harvester
